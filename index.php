ace08f2b32ea9303413221cea1cec8e2a024386b
Eris Television <eris-television@proton.me>
2022-09-23 05:16:36 +0200

    Adding $userName and $userMessage

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
+
+<?php $refreshTime = "32";?>
+
 <head>
     <meta charset="UTF-8"/>
-
     <title>Honigwabe Superchats</title>
 </head>
-<body>
-
-
-
-</body>
 
 <?php
-echo "Test";
+    $userName       = "Default-Name";
+    $userMessage    = "Default-Message";
+
 ?>
+
+<body>
+
+    <h1><?php echo $userName ?></h1>
+    <p><?php echo $userMessage ?></p>
+
+</body>
 
 </html> 
